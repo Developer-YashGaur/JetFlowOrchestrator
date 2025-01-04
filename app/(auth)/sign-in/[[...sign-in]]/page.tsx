220200edc0b16412f4548727c6677f301caece8a
foodiedeveloper <developer-yash-gaur@Yashs-MacBook-Pro.local>
2025-01-05 00:45:06 +0530

    changes

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,5 @@
-// import { SignIn } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs'
 import React from 'react'
-
-
-// new code
-
-import dynamic from 'next/dynamic';
-
-// Dynamically import Clerk's SignIn component with ssr: false to ensure client-side rendering
-const SignIn = dynamic(() => import('@clerk/nextjs').then(mod => mod.SignIn), { ssr: false });
-
-// --new code end
 
 export default function page() {
   return <SignIn />;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,5 @@
-// import { SignUp } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs'
 import React from 'react'
-
-// new code 
-
-import dynamic from 'next/dynamic';
-
-// Dynamically import Clerk's SignIn component with ssr: false to ensure client-side rendering
-const SignUp = dynamic(() => import('@clerk/nextjs').then(mod => mod.SignUp), { ssr: false });
 
 export default function page() {
   return (
